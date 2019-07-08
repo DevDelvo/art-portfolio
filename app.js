305993213df3990f25a57661eec83cc5c4349574
DevDelvo <kevinbdelvo@gmail.com>
2019-07-08 09:58:16 -0400

    created user schema and successful made POST of new user via postman

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require('dotenv').config();
+
+// import routes
+const userRoutes = require('./routes/user');
+
+// app
+const app = express();
+
+// db
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connected"));
+
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// routes middleware
+app.use('/api', userRoutes)
+
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
