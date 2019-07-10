d6dd248362e4a9729de26623b6c7306e8d281f7d
DevDelvo <kevinbdelvo@gmail.com>
2019-07-10 08:06:00 -0400

    successfully updated art with PUT request

--- controllers/art.js
+++ controllers/art.js
@@ -16,11 +16,6 @@ exports.artById = (req, res, next, id) => {
     });
 };
 
-exports.read = (req, res) => {
-    req.art.photo = undefined;
-    return res.json(req.art);
-}
-
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm() // form data is available here
     form.keepExtensions = true; // whatever images we are receiving, the extensions will be there
@@ -60,6 +55,53 @@ exports.create = (req, res) => {
         });
     });
 };
+
+exports.read = (req, res) => {
+    req.art.photo = undefined;
+    return res.json(req.art);
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm() // form data is available here
+    form.keepExtensions = true; // whatever images we are receiving, the extensions will be there
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded.'
+            });
+        }
+        // check for all fields
+        const { name, description, price, category, quantity, shipping } = fields
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required."
+            })
+        }
+
+        let art = req.art;
+        art = _.extend(art, fields);
+
+        if (files.photo) {
+            console.log("size", files.photo.size / 1000)
+            if (files.photo.size > 1000000) { // stops if file is more than 1MB (1000000KB) in size
+                return res.status(400).json({
+                    error: "Image should be less than 1MB in size."
+                });
+            }
+            art.photo.data = fs.readFileSync(files.photo.path)
+            art.photo.contentType = files.photo.type
+        }
+
+        art.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result);
+        });
+    });
+}
 
 exports.remove = (req, res) => {
     let art = req.art;
--- routes/art.js
+++ routes/art.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, artById, read, remove } = require('../controllers/art');
+const { create, artById, read, update, remove } = require('../controllers/art');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 
 router.get('/art/:artId', read)
-// router.put('/art/:artId/:userId', update);
+router.put('/art/:artId/:userId', requireSignin, isAuth, isAdmin, update);
 router.post('/art/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.delete('/art/:artId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.param('userId', userById)
