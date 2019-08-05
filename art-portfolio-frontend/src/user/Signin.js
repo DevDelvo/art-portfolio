35a46cd5a9c98abb09f29f259a14dccff7d6c564
DevDelvo <kevinbdelvo@gmail.com>
2019-08-05 19:19:15 -0400

    created react hook for user info state

--- art-portfolio-frontend/src/user/Signin.js
+++ art-portfolio-frontend/src/user/Signin.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../core/Layout';
 
 const Signin = () => {
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false,
+    });
+
+    const handleChange = name => e => {
+        setState({ ...state, error: false, [name]: e.target.value })
+    }
+
+    const handleSubmit = e => {
+        
+    }
+
     const signUpForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input type="text" className="form-control" />
+                <input type="name" className="form-control" onChange={handleChange('name')} />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input type="email" className="form-control" />
+                <input type="email" className="form-control" onChange={handleChange('email')} />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input type="password" className="form-control" />
+                <input type="password" className="form-control" onChange={handleChange('password')} />
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
@@ -25,6 +41,7 @@ const Signin = () => {
     return(
         <Layout title="Signin" description="Signin page" className="container col-md-8 offset-md-2">
             {signUpForm()}
+            {JSON.stringify(state)}
         </Layout>
     )
 }
