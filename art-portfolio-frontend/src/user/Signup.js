fd5deec05cff8ac567cf71231c83a421aeb2da42
DevDelvo <kevinbdelvo@gmail.com>
2019-08-05 19:02:04 -0400

    created config.js to store env variables more conveniently

--- art-portfolio-frontend/src/user/Signup.js
+++ art-portfolio-frontend/src/user/Signup.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Layout from '../core/Layout'
+import Layout from '../core/Layout';
+import { API } from '../config';
 
 const Signup = () => {
     return(
         <Layout title="Signup" description="Signup page">
-            
+            {API}
         </Layout>
     )
 }
