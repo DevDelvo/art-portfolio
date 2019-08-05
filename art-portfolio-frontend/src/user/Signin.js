8de237497832b9dc88e031b876a8ef135ed0c5c8
DevDelvo <kevinbdelvo@gmail.com>
2019-08-05 19:11:15 -0400

    created basic signup form

--- art-portfolio-frontend/src/user/Signin.js
+++ art-portfolio-frontend/src/user/Signin.js
@@ -2,9 +2,29 @@ import React from 'react';
 import Layout from '../core/Layout';
 
 const Signin = () => {
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input type="email" className="form-control" />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input type="password" className="form-control" />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    )
+
     return(
-        <Layout title="Signin" description="Signin page">
-            
+        <Layout title="Signin" description="Signin page" className="container col-md-8 offset-md-2">
+            {signUpForm()}
         </Layout>
     )
 }
