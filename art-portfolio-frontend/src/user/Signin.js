4e47833948e795e418bce65348db16ca70c7de07
DevDelvo <kevinbdelvo@gmail.com>
2019-08-05 19:51:10 -0400

    fix(accidentally had signup functions in signin) feature(successfully made post request via signup page frontend)

--- art-portfolio-frontend/src/user/Signin.js
+++ art-portfolio-frontend/src/user/Signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Layout from '../core/Layout';
+import axios from 'axios';
 
 const Signin = () => {
     const [state, setState] = useState({
@@ -10,38 +11,47 @@ const Signin = () => {
         success: false,
     });
 
-    const handleChange = name => e => {
-        setState({ ...state, error: false, [name]: e.target.value })
-    }
+    // const handleChange = name => e => {
+    //     setState({ ...state, error: false, [name]: e.target.value })
+    // }
 
-    const handleSubmit = e => {
-        
-    }
+    // const signin = async (name, email, password) => {
+    //     console.log(name, email, password)
+    //     try {
+    //         let res = await axios.post("https://localhost")
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // }
 
-    const signUpForm = () => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input type="name" className="form-control" onChange={handleChange('name')} />
-            </div>
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const {name, email, password} = state;
+    //     signin(name, email, password);
+    // }
 
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input type="email" className="form-control" onChange={handleChange('email')} />
-            </div>
+    // const signinForm = () => (
+    //     <form>
+    //         <div className="form-group">
+    //             <label className="text-muted">Name</label>
+    //             <input type="name" className="form-control" onChange={handleChange('name')} />
+    //         </div>
 
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input type="password" className="form-control" onChange={handleChange('password')} />
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
-    )
+    //         <div className="form-group">
+    //             <label className="text-muted">Email</label>
+    //             <input type="email" className="form-control" onChange={handleChange('email')} />
+    //         </div>
+
+    //         <div className="form-group">
+    //             <label className="text-muted">Password</label>
+    //             <input type="password" className="form-control" onChange={handleChange('password')} />
+    //         </div>
+    //         <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+    //     </form>
+    // )
 
     return(
         <Layout title="Signin" description="Signin page" className="container col-md-8 offset-md-2">
-            {signUpForm()}
-            {JSON.stringify(state)}
         </Layout>
     )
 }
--- art-portfolio-frontend/src/user/Signup.js
+++ art-portfolio-frontend/src/user/Signup.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../core/Layout';
+import axios from 'axios';
 import { API } from '../config';
 
 const Signup = () => {
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false,
+    });
+
+    const handleChange = name => e => {
+        setState({ ...state, error: false, [name]: e.target.value })
+    }
+
+    const signup = async (user) => {
+        console.log(user)
+        try {
+            let res = await axios.post(`${API}/signup`, user);
+            let { data } = res;
+            console.log(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const {name, email, password} = state;
+        const user = {name, email, password}
+        // console.log(user)
+        signup(user);
+    }
+
+    const signupForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="name" className="form-control" onChange={handleChange('name')} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input type="email" className="form-control" onChange={handleChange('email')} />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input type="password" className="form-control" onChange={handleChange('password')} />
+            </div>
+            <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+        </form>
+    )
+
+
     return(
-        <Layout title="Signup" description="Signup page">
-            {API}
+        <Layout title="Signup" description="Signup page"  className="container col-md-8 offset-md-2">
+            {signupForm()}
         </Layout>
     )
 }
