8f355fd634918d94d1ec7d71427f31456f35a31f
DevDelvo <kevinbdelvo@gmail.com>
2019-08-05 21:20:43 -0400

    successfully implemented backend errors to appear on frontend

--- art-portfolio-frontend/src/user/Signup.js
+++ art-portfolio-frontend/src/user/Signup.js
@@ -12,16 +12,15 @@ const Signup = () => {
         success: false,
     });
 
+    const {name, email, password, error, success} = state;
+
     const handleChange = name => e => {
         setState({ ...state, error: false, [name]: e.target.value })
     }
 
-    const signup = async (user) => {
-        console.log(user)
+    const signup = (user) => {
         try {
-            let res = await axios.post(`${API}/signup`, user);
-            let { data } = res;
-            console.log(data)
+            return axios.post(`${API}/signup`, user);
         } catch (err) {
             console.log(err)
         }
@@ -29,35 +28,76 @@ const Signup = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const {name, email, password} = state;
         const user = {name, email, password}
-        // console.log(user)
-        signup(user);
+        signup(user)
+        .then(() => {
+            setState({
+                ...state,
+                name: '',
+                email: '',
+                password: '',
+                error: '',
+                success: true,
+            })
+        })
+        .catch(error => {
+            let res = error.response;
+            setState({...state, error: res.data.error, success: false})
+        })
+        ;
     }
 
     const signupForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input type="name" className="form-control" onChange={handleChange('name')} />
+                <input
+                    type="name"
+                    className="form-control"
+                    onChange={handleChange('name')}
+                    value={name}
+                />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input type="email" className="form-control" onChange={handleChange('email')} />
+                <input
+                    type="email"
+                    className="form-control"
+                    onChange={handleChange('email')}
+                    value={email}
+                />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input type="password" className="form-control" onChange={handleChange('password')} />
+                <input
+                    type="password"
+                    className="form-control"
+                    onChange={handleChange('password')}
+                    value={password}
+                />
             </div>
             <button className="btn btn-primary" onClick={handleSubmit}>Submit</button>
         </form>
     )
 
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    )
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{display: success ? '' : 'none'}}>
+            New account created! Thank you for signing up!
+        </div>
+    )
 
     return(
         <Layout title="Signup" description="Signup page"  className="container col-md-8 offset-md-2">
+            {showSuccess()}
+            {showError()}
             {signupForm()}
         </Layout>
     )
