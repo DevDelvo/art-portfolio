04802725756fc3f3f0d65576d694df8a5573a79c
DevDelvo <kevinbdelvo@gmail.com>
2019-08-08 19:36:22 -0400

    added PrivateRoute fucntional component to handle redirects if user is not authenticated. Also note that the property pathname is LOWERCASE N

--- art-portfolio-frontend/src/Routes.js
+++ art-portfolio-frontend/src/Routes.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signin from './user/Signin';
 import Signup from './user/Signup';
 import Home from './core/Home'
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './user/UserDashboard';
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Switch>
-            <Route path="/" exact component={Home} />
+                <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
+                <PrivateRoute path="/dashboard" exact component={Dashboard} />
             </Switch>
         </BrowserRouter>
     )
