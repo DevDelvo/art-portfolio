d7f86b0a495fc69a6fe749fd62ebecd0cc12ac3e
DevDelvo <kevinbdelvo@gmail.com>
2019-08-16 03:16:42 -0400

    WIP(added handleToggle to Checkbox.js to toggle state of selected categories)

--- art-portfolio-frontend/src/core/Checkbox.js
+++ art-portfolio-frontend/src/core/Checkbox.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Checkbox = ({ categories }) => {
-  return categories.map((category, idx) => (
-    <li key={idx} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
-      <label className="form-check-label">{category.name}</label>
-    </li>
-  ));
+  const [checked, setChecked] = useState([]);
+
+  const handleToggle = cid => () => {
+    console.log(checked);
+    const currentCategoryId = checked.indexOf(cid); //returns first index or -1
+    console.log(currentCategoryId);
+    const newCheckedCategoryId = [...checked]; //gives all categories in the state
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(cid);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+  };
+
+  return categories.map((category, idx) => {
+    const { _id, name } = category;
+    return (
+      <li key={idx} className="list-unstyled">
+        <input
+          type="checkbox"
+          value={checked.indexOf(_id === -1)}
+          className="form-check-input"
+          onChange={handleToggle(_id)}
+        />
+        <label className="form-check-label">{name}</label>
+      </li>
+    );
+  });
 };
 
 export default Checkbox;
