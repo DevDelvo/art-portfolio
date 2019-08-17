03a158819af2e11e4b6428bf6c31bca5a349d10f
DevDelvo <kevinbdelvo@gmail.com>
2019-08-17 02:46:30 -0400

    set up braintree backend route and successfully generated token from braintree gateway

---
+++ controllers/braintree.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+const braintree = require('braintree');
+require('dotenv').config();
+
+const gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+  merchantId: process.env.BRAINTREE_MERCHANT_KEY
+});
+
+// google: braintree generate token node
+exports.generateToken = (req, res) => {
+  gateway.clientToken.generate({}, function(err, response) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
